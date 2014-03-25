8a8a2cc65736d0d9c0b91e4cebd2367afbfcb65e
meta-meta <paulmchristian@gmail.com>
2014-03-25 10:47:00 -0400

    map mandelbrot to hilbert. code is a bit messy

--- src/com/generalprocessingunit/processing/Main.java
+++ src/com/generalprocessingunit/processing/Main.java
@@ -7,44 +7,114 @@ import java.util.List;
 
 
 public class Main extends PApplet{
-	public static void main(String[] args){
-		PApplet.main(new String[] { /*"--present",*/ Main.class.getCanonicalName() });
+
+    static final int MAX_IT = 1000;
+
+    public static void main(String[] args){
+		PApplet.main(new String[] { "--present", Main.class.getCanonicalName() });
 	}
 
-    int n = 128; // n must be power of 2  \
-    int dMax = n*n-1;
+    static final int N = 2048; // N must be power of 2  \
+    static final int dMax = N * N -1;
     List<Vec> coords = new ArrayList();
-		
-	@Override
+
+    int[][] mandelbrot = new int[N][N];
+
+    List<Integer> mandelbrotOneD = new ArrayList<>();
+
+
+    @Override
 	public void setup() {		
-		size(1024, 1024, PApplet.OPENGL);
-
-
-        for(int d = 0; d < dMax; d++){
-            coords.add(d2xy(n,d));
-        }
+		size(1920, 1080, PApplet.OPENGL);
 
         background(0);
         colorMode(HSB);
 
+
+//        drawMandelbrot();
+        drawHilbert();
+    }
+
+    private void drawMandelbrot() {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                int it = getMandelVal(i, j);
+
+                stroke(200 + it % 255, 200, it == MAX_IT ? 0 : 200);
+                point(i * (width / (float) N), j * (height / (float) N));
+            }
+        }
+        //        For each pixel (Px, Py) on the screen, do:
+//        {
+//            x0 = scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.5, 1))
+//            y0 = scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1, 1))
+//
+//            x = 0.0
+//            y = 0.0
+//            iteration = 0
+//            max_iteration = 1000
+//            while ( x*x + y*y < 2*2  AND  iteration < max_iteration )
+//            {
+//                xtemp = x*x - y*y + x0
+//                y = 2*x*y + y0
+//                x = xtemp
+//                iteration = iteration + 1
+//            }
+//            color = palette[iteration]
+//            plot(Px, Py, color)
+//        }
+    }
+
+
+    private int getMandelVal(int i, int j) {
+        float x0 = -2.5f + i * (3.5f / N);
+        float y0 = -1f + j * (2f / N);
+
+        float x = 0f, y = 0f;
+        int it = 0;
+
+        while (x * x + y * y < 2 * 2 && it < MAX_IT)
+        {
+            float xtemp = x * x - y * y + x0;
+            y = 2 * x * y + y0;
+            x = xtemp;
+            it++;
+        }
+        return it;
+    }
+
+    private void drawHilbert()
+    {
+        for(int d = 0; d < dMax; d++){
+            Vec v = d2xy(N, d);
+            coords.add(v);
+
+            mandelbrotOneD.add(getMandelVal(v.x/8+1000,v.y/6+600));
+        }
+
+
         for(int d = 1; d < dMax; d++){
             Vec a = coords.get(d-1);
             Vec b = coords.get(d);
-            int s = 8;
-            stroke(d%100+100, 200, 40);
+            float s = 2.5f;
+            float sx = (width/((float) N)), sy = (height/(float) N);
+            strokeWeight(2);
+            stroke( (50 +  mandelbrotOneD.get(d) * 20 )%256, 255, mandelbrotOneD.get(d) == MAX_IT ? 0 : 255);
+//            line(a.x * sx, a.y * sy, b.x * sx, b.y * sy);
             line(a.x * s, a.y * s, b.x * s, b.y * s);
         }
+    }
 
-	}
-	
-	@Override
+    @Override
 	public void draw(){
-        int d = frameCount%(dMax-1) + 1;
-        Vec a = coords.get(d-1);
-        Vec b = coords.get(d);
-        int s = 8;
-        stroke(millis()%256, 200, 200);
-        line(a.x * s, a.y * s, b.x * s, b.y * s);
+//        int d = frameCount%(dMax-1) + 1;
+//        Vec a = coords.get(d-1);
+//        Vec b = coords.get(d);
+//        int s = 8;
+//        stroke(millis()%256, 200, 200);
+//        line(a.x * s, a.y * s, b.x * s, b.y * s);
 
 	}
 
